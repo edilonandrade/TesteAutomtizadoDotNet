404eddfc778a2228e614abc296f44a7727fe712e
Edilon Andrade <edilonandrade@gmail.com>
2019-04-26 19:29:12 -0300

    Incluindo o código da primeira video aula.

---
+++ Alura/TesteAutomatizado/TesteAutomatizado/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace TesteAutomatizado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            //IWebDriver driverC = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com.br/");
+
+            IWebElement campotexto = driver.FindElement(By.Name("q"));
+            campotexto.SendKeys("Caelum");
+
+            campotexto.Submit();
+
+            driver.Close();
+        }
+    }
+}
