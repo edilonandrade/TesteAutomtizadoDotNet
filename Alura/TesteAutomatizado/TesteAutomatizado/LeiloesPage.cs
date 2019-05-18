0aab417b66ba0bbfb1b65ef2b261a264e841ab61
Edilon Andrade <edilonandrade@gmail.com>
2019-05-17 21:41:17 -0300

    Demais arquivos da aula 4.

---
+++ Alura/TesteAutomatizado/TesteAutomatizado/LeiloesPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace TesteAutomatizado
+{
+    public class LeiloesPage
+    {
+        private IWebDriver driver;
+
+        public LeiloesPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void visita()
+        {
+            driver.Navigate().GoToUrl("http://localhost:8080/leiloes");
+        }
+
+        public NovoLeilaoPage novo()
+        {
+            driver.FindElement(By.LinkText("Novo Leilão")).Click();
+            return new NovoLeilaoPage(driver);
+        }
+
+        public bool existe(string produto, double valor, string usuario, bool usado)
+        {
+            return driver.PageSource.Contains(produto) &&
+                driver.PageSource.Contains(Convert.ToString(valor)) &&
+                driver.PageSource.Contains(usuario) &&
+                driver.PageSource.Contains(usado ? "Sim" : "Não");
+
+        }
+    }
+}
---
+++ Alura/TesteAutomatizado/TesteAutomatizado/LeiloesSystemTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+
+namespace TesteAutomatizado
+{
+    public class LeiloesSystemTest
+    {
+        private LeiloesPage leiloes;
+        private FirefoxDriver driver;
+
+        [SetUp]
+        public void inicializa()
+        {
+            driver = new FirefoxDriver();
+            leiloes = new LeiloesPage(driver);
+
+            UsuarioPage usuarios = new UsuarioPage(driver);
+            usuarios.visita();
+
+           
+        }
+
+        [Test]
+        public void deveCadastrarUmLeilao()
+        {
+            leiloes.visita();
+            NovoLeilaoPage novoLeilao = leiloes.novo();
+            novoLeilao.preenche("Geladeira", 123, "Paulo Henrique", true);
+        }
+    }
+}
